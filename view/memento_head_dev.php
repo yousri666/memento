6dac38440e71eaa78e2224312f0c1b982960620c
yousri <slama.yousri@gmail.com>
2017-06-23 22:09:40 +0200

    Initial commit

---
+++ view/memento_head_dev.php
@@ -0,0 +1,10 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+	<!--<link rel="stylesheet" href="bootstrap-select-1.12.2/css/bootstrap-select.css">
+	<link rel="stylesheet" href="bootstrap-toggle-master/css/bootstrap-toggle.css">-->
+	<link rel="stylesheet" href="css/memento.css">
+	<script src="angular-1.6.4/angular.js"></script>
+	<title>Memento</title>
+</head>
---
+++ view/memento_scripts_dev.php
@@ -0,0 +1,4 @@
+<script src="js/jquery-3.2.1.js"></script>
+<script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+<script src="jquery.i18n-master/src/jquery.i18n.js"></script>
+<script src="jquery.i18n-master/src/jquery.i18n.messagestore.js"></script>
